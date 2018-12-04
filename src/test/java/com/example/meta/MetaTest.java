4e6577d49e9260733ebe671a68baf08dc545de90
Mitchell Goshorn <msgoshorn@gmail.com>
2018-12-04 12:22:06 -0500

    Build pipeline test

---
+++ src/test/java/com/example/meta/MetaTest.java
@@ -0,0 +1,14 @@
+package com.example.meta;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MetaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
