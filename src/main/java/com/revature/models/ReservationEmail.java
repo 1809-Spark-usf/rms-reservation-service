21194eee0530b1b7658d39cfa868d93de2f36a2d
aldorotheo <aldorotheo@live.com>
2019-01-03 10:38:05 -0500

    Created object to transfer relevant information to the email service

---
+++ src/main/java/com/revature/models/ReservationEmail.java
@@ -0,0 +1,41 @@
+package com.revature.models;
+
+import java.time.LocalDateTime;
+
+/**
+ * The ReservationEmail Class
+ * Used to transfer information needed
+ * by the RMSEmailService in an object
+ * @author Austin D. 1811-Java-Nick 1/3/19 
+ *
+ */
+public class ReservationEmail {
+	/**The email of the user making the reservation*/
+	private String email;
+	/**The start time of the reservation*/
+	private LocalDateTime startTime;
+	/**The end time of the reservation*/
+	private LocalDateTime endTime;
+	/**The building name the reservation is taking place in*/
+	private String buildingName;
+	/**The resource name associated with the building*/
+	private String resourceName;
+	public ReservationEmail() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "ReservationEmail [email=" + email + ", startTime=" + startTime + ", endTime=" + endTime
+				+ ", buildingName=" + buildingName + ", resourceName=" + resourceName + "]";
+	}
+	public ReservationEmail(String email, LocalDateTime startTime, LocalDateTime endTime, String buildingName,
+			String resourceName) {
+		super();
+		this.email = email;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.buildingName = buildingName;
+		this.resourceName = resourceName;
+	}
+
+}
