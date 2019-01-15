205fdea075acffc506ae78f83320d2f2553a382f
David L. Batton <battondl@gmail.com>
2019-01-15 13:55:51 -0500

    added get reservation byid

--- src/main/java/com/revature/controllers/ReservationController.java
+++ src/main/java/com/revature/controllers/ReservationController.java
@@ -10,6 +10,7 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ import com.revature.enumerations.Purpose;
 import com.revature.enumerations.Type;
 import com.revature.models.Reservation;
 import com.revature.models.Resource;
+import com.revature.models.User;
 import com.revature.services.ReservationService;
 import com.revature.services.UserService;
 
@@ -130,8 +132,8 @@ public class ReservationController {
 	 * @param id The identification number for a resource.
 	 * @return A reservation. 
 	 */
-	@GetMapping("id")
-	public Reservation getReservationById(@RequestParam int id) {
+	@GetMapping("/{id}")
+	public Reservation getReservationById(@PathVariable("id") int id) {
 		return reservationService.getReservationById(id);
 	}
 	
@@ -211,5 +213,6 @@ public class ReservationController {
 	public List<Reservation> getAll() {
 		return reservationService.getAll();
 	}
+	
 
 }
